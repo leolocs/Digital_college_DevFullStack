a6b71dd0d1759e848442bda4e8a38554e2f67945
leolocs <leolocs04@gmail.com>
2025-05-06 21:51:29 -0300

    Atualização módulo javascript

---
+++ Módulo - Javascript/Calculadora/script.js
@@ -0,0 +1,20 @@
+
+function soma(){
+    resultado.innerHTML = Number(n1.value) + Number(n2.value)
+    sinal.innerHTML = "+"
+}
+
+function sub(){
+    resultado.innerHTML = Number(n1.value) - Number(n2.value)
+    sinal.innerHTML = "-"
+}
+
+function multi(){
+    resultado.innerHTML = Number(n1.value) * Number(n2.value)
+    sinal.innerHTML = "*"
+}
+
+function soma(){
+    resultado.innerHTML = Number(n1.value) + Number(n2.value)
+    sinal.innerHTML = "+"
+}
