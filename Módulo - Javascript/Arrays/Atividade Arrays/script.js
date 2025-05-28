66a9d8209a06c20498c7601789beb67e00c43545
unknown <leonardosouza1495@digitalcollege.internal>
2025-05-27 22:04:39 -0300

    Atualização aula de arrays

--- Módulo - Javascript/Arrays/Atividade Arrays/script.js
+++ Módulo - Javascript/Arrays/Atividade Arrays/script.js
@@ -1,10 +1,12 @@
 const calcular_horas_btn = document.querySelector('#calcular_horas_btn')
+const classificar_tarefa_btn = document.querySelector('#classificar_tarefa_btn')
 
 calcular_horas_btn.addEventListener('click', (e)=> {
     e.preventDefault();
     const horasTrabalhadas = Number(document.querySelector('#inputhorasTrabalhadas').value);
     const Valorhora = Number(document.querySelector('#inputValorhora').value);
     const resultadoCalculoHoras = document.querySelector('#resultadoCalculoHoras');
+
     // resultadoCalculoHoras = '';
     const total = Valorhora * horasTrabalhadas
     if(total >= 2000){
@@ -19,3 +21,25 @@ calcular_horas_btn.addEventListener('click', (e)=> {
 })
 
 
+classificar_tarefa_btn.addEventListener ('click', (e)=>{
+    e.preventDefault();
+    const atividade = document.querySelector('#atividade').value;
+    const prioridade = document.querySelector('#prioridade').value;
+    const resultado_tarefa = document.querySelector('#resultado_tarefa');
+
+    resultado_tarefa.textContent = '';
+
+    switch (prioridade){
+        case "1":
+            resultado_tarefa.textContent = `A atividade ${atividade} é urgente!`
+            break
+        case "2":
+            resultado_tarefa.textContent = `A atividade ${atividade} é importante!`
+            break
+        case "3":
+            resultado_tarefa.textContent = `A atividade ${atividade} é opcional!`
+            break
+    }
+
+    console.log(resultado_tarefa.textContent)
+})
